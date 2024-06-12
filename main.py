0873722ddb6382e8bb346d8dae67e59d9dfcec4a
santiagojorda <sjorda@fi.uba.ar>
2024-06-12 16:00:14 -0300

    readme

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
+from routes.support_routes import router as support_router
 
 app = FastAPI()
 
-class Tarea(BaseModel):
-    nombre: str # sirve para sincronizar los tipso de datos especificados en la clase
-    detalles: Optional[str]
+app.include_router(support_router, prefix="/support") # prefix es opcional
+
+
+
+# from pydantic import BaseModel
+# from typing import Optional
+# class Tarea(BaseModel):
+#     nombre: str # sirve para sincronizar los tipso de datos especificados en la clase
+#     detalles: Optional[str]
 
 # creamos la ruta con el decorador @ arriba de la funcion
-@app.get("/")
-def index():
-    return {'mensaje': 'squad5'}
+# @app.get("/")
+# def index():
+#     return {'mensaje': 'squad5'}
 
 
-@app.get("/tareas/{id}")
-def mostrar_tarea(id: int):
-    return {'tarea': id}
+# @app.get("/tareas/{id}")
+# def mostrar_tarea(id: int):
+#     return {'tarea': id}
 
-@app.post("/tarea")
-def insertar_tarea(tarea: Tarea):
-    return {'mensaje': f"tarea {tarea.nombre} insertado"}
+# @app.post("/tarea")
+# def insertar_tarea(tarea: Tarea):
+#     return {'mensaje': f"tarea {tarea.nombre} insertado"}

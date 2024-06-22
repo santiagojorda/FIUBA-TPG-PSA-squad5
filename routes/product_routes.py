e33f925b28cf09eefbac37484e6c0d4fb2ae7376
santiagojorda <sjorda@fi.uba.ar>
2024-06-22 17:28:27 -0300

    endpoints con handler de errores

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,35 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services.product_service import Product_service
+from services.version_service import Version_service
 
 PATH = "/products"
 
 router = APIRouter()
 product_service = Product_service() 
+version_service = Version_service() 
 
- # Devuelve todos los productos
+# Devuelve todos los productos
 @router.get("/")
 async def get_products():
     products = product_service.get_products()
     return {"products": products}
 
- # Devuelve un producto buscado por su ID
+# Devuelve un producto buscado por su ID
 @router.get("/{product_id}")
 async def get_product(product_id: int):
     product = product_service.get_product(product_id)
-    # if not product:
-        # raise HTTPException(status_code=404, detail="Product not found")
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
     return {"product": product}
 
-@router.get("/products/{product_id}/v/{version_id}")
-async def get_products():
-    products = product_service.get_products()
-    return {"products": products}
+# Devuelve el producto y su version
+@router.get("/{product_id}/v/{version_id}")
+async def get_version_of_a_product(product_id, version_id):
+    version = version_service.get_version_by_product_id(product_id, version_id)
+    if not version:
+        raise HTTPException(status_code=404, detail="Version not found")
+    return {"version": version}
 
 
 # Devuelve el producto con id con todas sus versiones
-@router.get("/{product_id}")
-async def index(product_id):
-    return {'mensaje': f"devuelve el producto con id {product_id}"}
+@router.get("/{product_id}/v/")
+async def get_versions_of_a_product(product_id):
+    versions = version_service.get_versions_by_product_id(product_id)
+    if not versions:
+        raise HTTPException(status_code=404, detail="Versions not found")
+    return {"versions": versions}
 
 # Devuelve el producto con titulo con todas sus versiones
 @router.get("/title/{product_title}")
--- services/version_service.py
+++ services/version_service.py
@@ -1,15 +1,20 @@
 from res.database import db
 
+from models.version import Version
+
 class Version_service():
 
     def __init__(self):
         self.db = db
 
-    def get_version(self, version_id: int):
-        return self.db.get_session().query(Version).filter(Version.id_version == version_id).first()
+    def get_version(self, version_code: int):
+        return self.db.get_session().query(Version).filter(Version.version_code == version_code).first()
 
     def get_versions(self):
         return self.db.get_session().query(Version).all()
 
     def get_versions_by_product_id(self, product_id: int):
-        return self.db.get_session().query(Version).filter(Version.id_product == product_id).all()    
+        return self.db.get_session().query(Version).filter(Version.product_id == product_id).all()    
+    
+    def get_version_by_product_id(self, product_id: int, version_code: str):
+        return self.db.get_session().query(Version).filter(Version.product_id == product_id, Version.version_code == version_code).first()    

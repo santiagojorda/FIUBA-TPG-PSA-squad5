c05fca4ef06968b54839738833687625975867c3
Jburaschi <jburaschi@fi.uba.ar>
2024-06-22 22:06:08 -0300

    Modificaciones

--- routes/incident_routes.py
+++ routes/incident_routes.py
@@ -7,15 +7,15 @@ incident_service = Incident_service()
 
 PATH = "/incident"
 
-# Devuelve todos las querys
+# Devuelve todos los incidentes
 @router.get("/")
-async def get_Query():
+async def get_incidents():
     querys = incident_service.get_Incidents()
     return {"querys": querys}
 
 # Devuelve un query buscado por id de ticket
 @router.get("/incident/{severity_id}")
-async def get_Query(Ticket_id: int):
+async def get_Incident(Ticket_id: int):
     querys = incident_service.get_Incident(Ticket_id)
     if not querys:
         raise HTTPException(status_code=404, detail="incident not found")

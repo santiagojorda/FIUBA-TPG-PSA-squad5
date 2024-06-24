798122369de9239745b896516e96d48d788d88fd
santiagojorda <sjorda@fi.uba.ar>
2024-06-23 23:30:21 -0300

    chau severities

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routes.tickets_routes import PATH as TICKETS_PATH, TICKET_TAG, router as tickets_router
 from routes.product_version_routes import PATH as PRODUCT_PATH, PRODUCT_TAG, router as product_versions_router
-from routes.severity_routes import PATH as SEVERITY_PATH, SEVERITY_TAG, router as severity_router
 from routes.client_routes import PATH as CLIENT_PATH, CLIENT_TAG, router as client_router
 from routes.task_routes import PATH as TASK_PATH, TASK_TAG, router as tasks_router
 # from routes.query_routes import PATH as QUERY_PATH, router as query_router
@@ -19,7 +18,6 @@ db_support = Database(SQLALCHEMY_DATABASE_URL)
 
 app.include_router(tickets_router, prefix=TICKETS_PATH, tags=[TICKET_TAG]) # prefix es opcional
 app.include_router(product_versions_router, prefix=PRODUCT_PATH, tags=[PRODUCT_TAG]) # prefix es opcional
-app.include_router(severity_router, prefix=SEVERITY_PATH, tags=[SEVERITY_TAG]) # prefix es opcional
 app.include_router(client_router, prefix=CLIENT_PATH, tags=[CLIENT_TAG]) # prefix es opcional
 app.include_router(tasks_router, prefix=TASK_PATH, tags=[TASK_TAG]) # prefix es opcional
 

f210770fa4992b788defa4091a5ed2a56714174c
Jburaschi <jburaschi@fi.uba.ar>
2024-06-30 10:24:50 -0300

    Test eliminacón de ticket

--- tests/features/steps/u6_eliminación_ticket.py
+++ tests/features/steps/u6_eliminación_ticket.py
@@ -1,5 +1,4 @@
 from behave import *
-
 from services.tickets_service import Ticket_service
 from routes.tickets_routes import PATH as ENDPOINT_TICKETS
 from res.errors import Product_not_exist_exception, Invalid_data_exception, Version_code_not_exist_exception
@@ -14,6 +13,7 @@ ticket_service = Ticket_service()
 
 def create_query_ticket(product_id, version_code):
     return Ticket(
+        id = 1, 
         product_id = product_id,
         version_code = version_code,
         title = MOCK_TICKET_TITLE,
@@ -24,24 +24,22 @@ def create_query_ticket(product_id, version_code):
         response = MOCK_TICKET_RESPONSE,
         opening_date = MOCK_OPENING_DATE
     )
+
+MESSAGE_TICKET_DELETED = "Ticket deleted successfully"
+
 #-- Escenario 1
 
 @given("se ingreso un id de un ticket valido")
 def create_valid_query_ticket(context):
-
     existing_product_id, existing_version_code = create_version_and_product_1() 
     context.expected_ticket = create_query_ticket(existing_product_id, existing_version_code)
-    context.original_ticket_id = context.expected_ticket.id  # Guardar el ID del ticket original para futuras referencias
     ticket_service.create_ticket(context.expected_ticket)
 
 @when("se Elimina un ticket")
 def eliminar_ticket(context):
-    
-    context.response = context.client.delete(f"{context.expected_ticket}/{context.expected_ticket.product_id}/{context.expected_ticket.version_code}/{context.expected_ticket.id}")
-
+    context.response = context.client.delete(f"{ENDPOINT_TICKETS}/{context.expected_ticket.product_id}/{context.expected_ticket.version_code}/{context.expected_ticket.id}")
 
 @then('se Elimina el ticket y le informa al usuario que se hizo correctamente')
 def step_impl(context):
     response = context.response
     assert response.json()['message'] == MESSAGE_TICKET_DELETED  # Verificar el mensaje de confirmación
-

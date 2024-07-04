a2d5ebbdd5a656e90204bf156e864b4d16af86d8
santiagojorda <sjorda@fi.uba.ar>
2024-07-04 00:48:51 -0300

    ✳️ Datos de prueba - Mocks

--- res/mocks/version_products.py
+++ res/mocks/version_products.py
@@ -1,11 +1,12 @@
 mock_products = [
     {'title': 'Siu-Guarani'},
     {'title': 'MercadoLibre'},
-    {'title': 'Olx'}
+    {'title': 'olx'}
 ]
 
 mock_versions = [
     {'version_code': "1.1.0",  'product_id': 1, 'release_notes': 'Email comunication'},
     {'version_code': "2.2.0",  'product_id': 1, 'release_notes': 'Exam notes app'},
-    {'version_code': "2.2.03", 'product_id': 3, 'release_notes': 'mi primera version'}
+    {'version_code': "2.0.0",  'product_id': 2, 'release_notes': 'Shopping cart'},
+    {'version_code': "2.2.3", 'product_id': 3, 'release_notes': 'Shopping cart'}
 ]
